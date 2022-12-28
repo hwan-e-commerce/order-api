b5688804724efcd7dfca4b7675cc86ddaa6a7b3c
devbloo <zpswl45@naver.com>
2022-12-28 19:56:50 +0900

    feat: logging

--- src/main/java/co/hwan/order/interfaces/healthcheck/HealthCheckApiController.java
+++ src/main/java/co/hwan/order/interfaces/healthcheck/HealthCheckApiController.java
@@ -2,17 +2,21 @@ package co.hwan.order.interfaces.healthcheck;
 
 
 import co.hwan.order.interfaces.healthcheck.dto.response.HealthCheckApiFormat;
+import java.time.LocalDateTime;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequestMapping("/api/health-check")
 @RestController
 public class HealthCheckApiController {
 
     @GetMapping
     ResponseEntity<HealthCheckApiFormat> healthCheck() {
+        log.info("current time: {}", LocalDateTime.now());
         return ResponseEntity.ok(HealthCheckApiFormat.of("OK V4"));
     }
 }
