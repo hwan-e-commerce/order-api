09923d038452c48ff3a56d5fe24e18e14570bfe5
devbloo <zpswl45@naver.com>
2023-01-01 18:07:29 +0900

    feat: order entity추가

--- src/main/java/co/hwan/order/domain/order/Order.java
+++ src/main/java/co/hwan/order/domain/order/Order.java
@@ -1,5 +1,35 @@
 package co.hwan.order.domain.order;
 
-public class Order {
+import co.hwan.order.domain.Timestamp;
+import java.time.LocalDateTime;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+
+public class Order extends Timestamp {
+    protected Order() {}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderToken;
+    private Long userId;
+    private String payMethod;
+    private LocalDateTime orderAt;
+    private Status status;
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum Status {
+        INIT("주문시작"),
+        ORDER_COMPLETE("주문완료"),
+        DELIVERY_PREPARE("배송준비"),
+        IN_DELIVERY("배송중"),
+        DELIVERY_COMPLETE("배송완료");
+
+        private final String description;
+    }
 }
