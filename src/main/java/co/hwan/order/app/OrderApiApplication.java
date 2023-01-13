f7a86f571615521f0a3a687edc6fe541a26eb4f0
devbloo <zpswl45@naver.com>
2023-01-13 18:09:18 +0900

    chore: redis config

--- src/main/java/co/hwan/order/app/OrderApiApplication.java
+++ src/main/java/co/hwan/order/app/OrderApiApplication.java
@@ -1,21 +1,13 @@
 package co.hwan.order.app;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
 @SpringBootApplication
 public class OrderApiApplication {
-    @Value("${spring.datasource.url}")
-    static private String jdbcHost;
-//    @Value("${spring.redis.host}")
-//    static private String redisHost;
-
     public static void main(String[] args) {
         SpringApplication.run(OrderApiApplication.class, args);
-        log.info("==================");
-        log.info("jdbc url: {}", jdbcHost);
     }
 }
