e964bf1b9f6289e39d0c9bf111a6572391afe446
devbloo <zpswl45@naver.com>
2023-02-14 20:24:24 +0900

    chore: loggig code 추가

--- src/main/java/co/hwan/order/app/item/stock/service/StockService.java
+++ src/main/java/co/hwan/order/app/item/stock/service/StockService.java
@@ -49,12 +49,13 @@ public class StockService {
 
         ValueOperations<String, String> stringValueOperations = stringRedisTemplate.opsForValue();
         String res = stringValueOperations.get(item.getItemToken());
-
+        log.info("stock cnt: {}" + res);
         if(res == null) {
             throw new InvalidParamException("등록된 재고가 없습니다.");
         }
 
         int i = Integer.parseInt(res) - quantity;
+        log.info("stock remain: {}" + res);
         stringValueOperations.set(itemToken, String.valueOf(i));
     }
 }
